cd89eede8bcbd9f4ef1376c9894660251c55f129
Brocel <geenens.prof@gmail.com>
2021-03-03 16:07:55 -0300

    Edit app and server, example with stuffs in API

--- app.js
+++ app.js
@@ -8,24 +8,46 @@ const app = express();
 
 //     res.json({ message: 'Your request was sucessful !' });
 // });
-// Exemple 2 : (with next)
-app.use((req, res, next) => {
-    console.log('Request received !');
-    next();
-});
+// Exemple 2 : (middleware with next)
+// app.use((req, res, next) => {
+//     console.log('Request received !');
+//     next();
+// });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next;
-});
+// app.use((req, res, next) => {
+//     res.status(201);
+//     next;
+// });
 
-app.use((req, res, next) => {
-    res.json({ message: 'Your request was sucessful !' });
-    next();
-});
+// app.use((req, res, next) => {
+//     res.json({ message: 'Your request was sucessful !' });
+//     next();
+// });
 
-app.use((req, res, next) => {
-    console.log('Response sent successfully !');
+// app.use((req, res, next) => {
+//     console.log('Response sent successfully !');
+// });
+
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzoei',
+            title: 'My first thing', 
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'qsomihvqios',
+        }, 
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
 
 // export app as a module
