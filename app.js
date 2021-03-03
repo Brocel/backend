0de184bf4b425c326abc2a520d349bf138275f3c
Brocel <geenens.prof@gmail.com>
2021-03-03 12:39:36 -0300

    Edit app and server.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express'); // import express package
 
 const app = express();
 
+// app response, to see if everything is working
+app.use((req, res) => {
 
+    res.json({ message : 'Your request was sucessful !'});
+});
 
+// export app as a module
 module.export = app;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require('http'); // importing http package
+const app = require('./app'); // importing app 
+
 
 // func to create server
-const server = http.createServer((req, res) =>{
-
-    res.end('This is my server response');
-
-});
+//const server = http.createServer((req, res) =>{
+//
+//   res.end('This is my server response');
+//
+//});
+app.set('port', process.env.PORT || 3000);
+const server = htpp.createServer(app);
 
 // set the server to listen on port 3000 for dev
 server.listen(process.env.port || 3000);
