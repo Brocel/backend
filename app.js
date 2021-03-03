484fe853b958e8ad54525d58649513589432e827
Brocel <geenens.prof@gmail.com>
2021-03-03 15:25:41 -0300

    Edit app.js, next()

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require('express'); // import express package
 
 const app = express();
 
-// app response, to see if everything is working
-app.use((req, res) => {
+// app use cycle
+// Exemple 1 : 
+// app.use((req, res) => {
 
-    res.json({ message : 'Your request was sucessful !'});
+//     res.json({ message: 'Your request was sucessful !' });
+// });
+// Exemple 2 : (with next)
+app.use((req, res, next) => {
+    console.log('Request received !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next;
+});
+
+app.use((req, res, next) => {
+    res.json({ message: 'Your request was sucessful !' });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Response sent successfully !');
 });
 
 // export app as a module
